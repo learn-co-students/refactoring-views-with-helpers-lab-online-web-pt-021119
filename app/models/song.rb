b3247995e3f2d635991fbf78450b518f51da4020
cyantis <cyantis@gmail.com>
2019-07-15 19:43:05 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self.artist = Artist.new
+    self.artist.name = name
+    self.save
   end
 end
